c5454c1e2051cffa5bab764086781db8e28aa0f7
unknown <valikpalica>
2021-06-07 19:33:20 +0300

    last

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const autification = require('./route/mainRoute');
-
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
-const  parser = require('body-parser');
+const parser = require('body-parser');
 const Activedb = require('./API/dbUsers/Activedb');
 const host = '192.168.1.103';
 app.set("views engine", "hbs");
